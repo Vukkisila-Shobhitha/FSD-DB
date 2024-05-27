9525600a702ca15be5c83fd1db62638bbc8053e4
Vukkisila-Shobhitha <shobitha6294@gmail.com>
2024-05-27 17:27:20 +0530

    connecting to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 //import the app module
 const app = require('./app');
 
-app.listen(3001, () => {
-  console.log("Server is running on http://localhost:3001");
-});
+//import the mongoose module
+const mongoose = require('mongoose');
+
+//log the message connecting to the MongoDB
+console.log('Connecting to MongoDB...');
+
+mongoose.connect(`mongodb+srv://shobitha6294:shobi6294@mongodb1.wvf3nob.mongodb.net/`)
+ .then(
+    () => {
+        console.log('Connected to MongoDB');
+
+        // start the server
+      app.listen(3001, () => {
+       console.log("Server is running on http://localhost:3001");
+      });
+    }
+ )
+ .catch((error) => {
+    console.log('Error connecting to MongoDB: ', error.message);
+ });
+ 
