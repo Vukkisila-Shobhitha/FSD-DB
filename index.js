0d899694565af5eb77b51e8c09972046f866253e
Vukkisila-Shobhitha <162134462+Vukkisila-Shobhitha@users.noreply.github.com>
2024-06-01 15:04:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,11 @@ const mongoose = require('mongoose');
 //log the message connecting to the MongoDB
 console.log('Connecting to MongoDB...');
 
-<<<<<<< HEAD
+
 // import config module
 const config = require('./utils/config');
 
-
 mongoose.connect(config.MONGODB_URI)
-=======
-//import the dotenv module
-require('dotenv').config();
-
-
-mongoose.connect(process.env.MONGODB_URI)
->>>>>>> 0b815c40779e97aa1319ec5315b809ae3c59cdff
  .then(
     () => {
         console.log('Connected to MongoDB');
