47c1fb99d0f19fbf31ddf93f24481f023b4da973
Vukkisila-Shobhitha <shobitha6294@gmail.com>
2024-06-01 14:52:04 +0530

    added user model

--- app.js
+++ app.js
@@ -8,14 +8,5 @@ app.use(express.json());
 //middleware to log the request
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send('get request to the homepage');
-});
-
-app.post('/',(req, res) => {
-  //console.log(req.body);
-  res.send('post request to the homepage');
-});
-
 //export the app module
 module.exports = app;
