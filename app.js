0b815c40779e97aa1319ec5315b809ae3c59cdff
Vukkisila-Shobhitha <162134462+Vukkisila-Shobhitha@users.noreply.github.com>
2024-06-01 13:29:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+//middleware to parse the request body
+app.use(express.json());
+
+//middleware to log the request
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('get request to the homepage');
+});
+
+app.post('/',(req, res) => {
+  //console.log(req.body);
+  res.send('post request to the homepage');
+});
+
+//export the app module
+module.exports = app;
